466826ee930ea12d8147bbab02a58b9501bc3099
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-10-20 21:38:58 +0530

    fix

--- routes/Department.js
+++ routes/Department.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const departmentController = require("../controller/DepartmentController");
+const departmentController = require("../controller/Department");
 
 // Add a new department
 router.post("/", departmentController.addDepartment);
