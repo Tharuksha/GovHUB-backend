eb5ae0d73cda135073181d434facb7b8c37cc00f
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-10-22 02:43:31 +0530

    fix

--- routes/Customer.js
+++ routes/Customer.js
@@ -1,26 +1,181 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const customerController = require('../controller/Customer');
+const customerController = require("../controller/Customer");
 
-// Add a new customer
-router.post('/', customerController.addCustomer);
+/**
+ * @swagger
+ * /api/customers:
+ *   post:
+ *     summary: Add a new customer
+ *     tags: [Customers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - emailAddress
+ *               - password
+ *               - firstName
+ *               - lastName
+ *             properties:
+ *               emailAddress:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               phoneNumber:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Customer added successfully
+ *       400:
+ *         description: Error adding customer
+ */
+router.post("/", customerController.addCustomer);
 
-// Customer login
-router.post('/login', customerController.login);
+/**
+ * @swagger
+ * /api/customers/login:
+ *   post:
+ *     summary: Customer login
+ *     tags: [Customers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Invalid credentials
+ *       404:
+ *         description: Customer not found
+ */
+router.post("/login", customerController.login);
 
-// Get all customers
-router.get('/', customerController.getCustomers);
+/**
+ * @swagger
+ * /api/customers:
+ *   get:
+ *     summary: Get all customers
+ *     tags: [Customers]
+ *     responses:
+ *       200:
+ *         description: List of all customers
+ *       500:
+ *         description: Server error
+ */
+router.get("/", customerController.getCustomers);
 
-// Get a single customer by ID
-router.get('/:id', customerController.getCustomerById);
+/**
+ * @swagger
+ * /api/customers/{id}:
+ *   get:
+ *     summary: Get a customer by ID
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Customer details
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Server error
+ */
+router.get("/:id", customerController.getCustomerById);
 
-// Get a single customer by e-mail
-router.get('/email/:email', customerController.getCustomerByEmail);
+/**
+ * @swagger
+ * /api/customers/email/{email}:
+ *   get:
+ *     summary: Get a customer by email
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: email
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Customer details
+ *       404:
+ *         description: Customer not found
+ *       400:
+ *         description: Bad request
+ */
+router.get("/email/:email", customerController.getCustomerByEmail);
 
-// Update a customer
-router.put('/:id', customerController.updateCustomer);
+/**
+ * @swagger
+ * /api/customers/{id}:
+ *   put:
+ *     summary: Update a customer
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     responses:
+ *       200:
+ *         description: Customer updated successfully
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Server error
+ */
+router.put("/:id", customerController.updateCustomer);
 
-// Delete a customer
-router.delete('/:id', customerController.deleteCustomer);
+/**
+ * @swagger
+ * /api/customers/{id}:
+ *   delete:
+ *     summary: Delete a customer
+ *     tags: [Customers]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Customer deleted successfully
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Server error
+ */
+router.delete("/:id", customerController.deleteCustomer);
 
 module.exports = router;
